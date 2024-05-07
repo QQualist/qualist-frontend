9945f695d9c6aea773876b09ad215cbb77ed8a40
ViniciusKuchnir <vina200902@gmail.com>
2024-05-06 21:27:53 -0300

    feat: Add shadcnUI to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
 
 const App = () => {
 
   return (
     <>
       <h1 className="text-2xl text-blue-800 font-bold">Hello Qualist</h1>
+      <Button className='ml-2' variant='default'>Shadcn UI</Button>
     </>
   )
 }
