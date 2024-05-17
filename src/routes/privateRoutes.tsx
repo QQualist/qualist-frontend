5894b9d02e597de43130b750413e59cfb6996564
ViniciusKuchnir <vina200902@gmail.com>
2024-05-16 23:54:28 -0300

    feat: Creating application routes

---
+++ src/routes/privateRoutes.tsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/user";
+import { ContextUser } from "../types/ContextUser";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoutes = () => {
+  const { signed } = useContext(UserContext) as ContextUser;
+
+  return signed ? <Outlet /> : <Navigate to="/signin" />;
+};
+
+export default PrivateRoutes;
