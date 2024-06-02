8f58b68d93e9194fcc96d655c2427840acb7d11b
Gustavo Guimarães Martins <90704921+GustavoGuimaraes32@users.noreply.github.com>
2024-06-02 04:02:47 -0300

    Style: Home Responsiveness

--- src/views/home/functionalities.tsx
+++ src/views/home/functionalities.tsx
@@ -7,7 +7,7 @@ const Functionalities = () => {
     const { t } = useTranslation();
 
     return (
-        <section className='px-24'>
+        <section className='px-5 md:px-10 lg:px-24'>
             <div className='mt-32'>
                 <h1 className='font-bold text-3xl text-dark-blue leading-tight text-center dark:text-white'>{t("Discover Qualist’s")} <span className='text-light-blue'>{t("Features")}</span></h1>
                 <p className='text-dark-gray text-center mt-5 dark:text-light-gray'>{t("Below, we present the main features that make Qualist the")} <br /> {t("ideal choice for simplifying and optimizing your software quality audits.")}</p>
@@ -15,10 +15,10 @@ const Functionalities = () => {
 
             <div className='flex flex-wrap justify-center mt-20'>
                 {ListFuncItems.map((item) => (
-                    <div className='w-1/4 flex flex-col items-center bg-gray-100 p-12 rounded-lg m-5 bg-light-gray bg-opacity-5'>
+                    <div className=' flex flex-col items-center bg-gray-100 p-12 rounded-lg m-5 bg-light-gray bg-opacity-5 w-full md:w-1/2 lg:w-1/4'>
                         <item.icon className="text-7xl text-light-gray dark:text-white"/>
-                        <h2 className='font-bold text-xl text-light-blue mt-5 text-center'>{item.text}</h2>
-                        <p className='text-dark-gray text-center mt-5 dark:text-light-gray'>{item.description}</p>
+                        <h2 className='font-bold text-xl text-light-blue mt-5 text-center'>{t(item.text)}</h2>
+                        <p className='text-dark-gray text-center mt-5 dark:text-light-gray'>{t(item.description)}</p>
                     </div>
                 ))}
             </div>
--- src/views/home/main.tsx
+++ src/views/home/main.tsx
@@ -11,18 +11,18 @@ const Main = () => {
 
     return (
 
-        <main className='ml-24 p-0 flex justify-center items-center'>
+        <main className='text-center m-5 md:m-10 lg:ml-24 lg:text-start p-0 flex justify-center items-center'>
 
-            <div className="left-side w-1/2">
-                <h1 className='font-bold text-6xl text-dark-blue leading-tight dark:text-white'><span className='text-light-blue'>Qualist</span> - {t("Elevating the Standard of Software Quality")}</h1>
-                <p className='text-dark-gray font-normal text-lg mt-2.5 mr-0 mb-12 ml-0 dark:text-light-gray'>
+            <div className="left-side w-full lg:w-1/2">
+                <h1 className='font-bold text-4xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-dark-blue leading-tight dark:text-white'><span className='text-light-blue'>Qualist</span> - {t("Elevating the Standard of Software Quality")}</h1>
+                <p className='text-dark-gray font-normal mt-2.5 mr-0 mb-12 ml-0 text-sm lg:text-sm lg:mb-5 xl:text-base 2xl:text-lg dark:text-light-gray'>
                     {t("Qualist is the essential tool for QA professionals who want to transform software quality management. With a user-friendly interface and advanced features, Qualist makes it easy to create checklists, conduct audits, and manage nonconformities, providing a more efficient and accurate experience.")}
                 </p>
                 <Button className='h-12 px-10 text-lg font-normal dark:shadow-externo' type='submit'><Link to={"/signup"}>{t("Become a customer")}</Link></Button>
             </div>
 
-            <div className="right-side w-1/2">
-                <img className='w-[88%]' src={MainImage} alt="Woman using a Notebook" />
+            <div className="right-side w-0 lg:w-1/2">
+                <img className='hidden lg:block lg:w-[45%] 2xl:w-2/5 absolute bottom-0' src={MainImage} alt="Woman using a Notebook" />
             </div>
         </main>
     )
