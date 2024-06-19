7fdbb06eadfa93fdd4bb3e37805a51c4a98b73cc
ViniciusKuchnir <vina200902@gmail.com>
2024-06-18 22:40:03 -0300

    fix: Add props in Checkbox component

--- src/components/Inputs/Checkbox/CheckboxInput.tsx
+++ src/components/Inputs/Checkbox/CheckboxInput.tsx
@@ -1,15 +1,20 @@
 import { Checkbox } from "@/components/ui/checkbox";
+import { CheckboxProps } from "@radix-ui/react-checkbox";
 import { UseFormRegisterReturn } from "react-hook-form";
 
-interface ICheckboxInput {
+interface ICheckboxInput extends CheckboxProps {
   id: string;
+  checked: boolean | undefined;
+  onCheckedChange: (e: boolean) => void;
   register?: UseFormRegisterReturn;
 }
 
-const CheckboxInput = ({ id, register }: ICheckboxInput) => {
+const CheckboxInput = ({ id, register, checked, onCheckedChange }: ICheckboxInput) => {
   return (
     <Checkbox 
         id={id}
+        checked={checked}
+        onCheckedChange={(e) => onCheckedChange(e as boolean)}
         {...register}
     />
   );    
