c8e99c15c87a163ecabf4507b9ea316258776d8d
ViniciusKuchnir <vina200902@gmail.com>
2024-06-25 21:48:58 -0300

    feat: List the reminders of the audit

---
+++ src/utils/getRemindersByAudit.ts
@@ -0,0 +1,15 @@
+import { getUserToken } from "./getUserToken";
+import { api } from "@/api/api";
+import { AuditReminderData } from "@/types/audit-reminder";
+
+export const getRemindersByAudit = async (audit_uuid: string): Promise<AuditReminderData[]> => {
+  const userToken = getUserToken();
+
+  const audit_reminders = await api.get(`audit-reminders/audits/${audit_uuid}`, {
+    headers: {
+      Authorization: `Bearer ${userToken}`,
+    },
+  });
+    console.log(audit_reminders.data)
+  return audit_reminders.data;
+};
